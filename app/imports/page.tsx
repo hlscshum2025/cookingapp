5f4e64671cc5a1b434e362fed8cce7ee98e5f0fb
hlscshum2025 <caoyue.shum@gmail.com>
2026-08-13 15:48:45 +0800

    Expose shared source import in import center

--- app/imports/page.tsx
+++ app/imports/page.tsx
@@ -1,5 +1,9 @@
 import { ImportWorkspace } from "@/components/ImportWorkspace";
+import { UniversalSourceImport } from "@/components/UniversalSourceImport";
 
 export default function ImportsPage(){
-  return <ImportWorkspace/>;
+  return <>
+    <div className="page" style={{paddingBottom:0}}><UniversalSourceImport/></div>
+    <ImportWorkspace/>
+  </>;
 }
